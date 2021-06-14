aeb3aceb8abe001203aad02b23a51e155d53d665
Zil Norvilis <zilasino27@gmail.com>
2021-06-14 10:28:14 +0200

    Refactor filter reducer

--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,23 +1,12 @@
-import React from 'react'
-import books from './books'
-const filter = () => {
-  const bookFilter = () => {
-    books.filter((book,id,self) => {
-      return self.indexOf(book) === id
-    })
+export const CHANGE_FILTER = "CHANGE_FILTER";
 
+const initialState = "All";
+
+const filterReducer = (state = initialState, action) => {
+  if (action.type === CHANGE_FILTER) {
+    return action.filter;
   }
-  return (
-    <div>
-      {books.map((book) => (
-        <div key={book.id}>{book.title}
-        <button onClick={() => bookFilter(book.id)}>filter</button>
-        </div>
-      ))
+  return state;
+};
 
-      }
-    </div>
-  )
-}
-
-export default filter;
+export default filterReducer;
