9ba9f1498f7b7a34ca0442380173adf0bf68db5c
Zil Norvilis <zilasino27@gmail.com>
2021-06-14 17:14:25 +0200

    Refactor index.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import BooksList from '../containers/BooksList';
 import BooksForm from '../containers/BooksForm';
-import '../styles/index.css';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <BooksList />
       <BooksForm />
     </div>
